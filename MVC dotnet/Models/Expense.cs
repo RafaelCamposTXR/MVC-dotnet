6bb0cf7a68bc39deb0e41ace395d1415bd133fd3
RafaelCamposTXR <102463380+RafaelCamposTXR@users.noreply.github.com>
2024-10-10 15:37:58 -0300

    Adicionar arquivos de projeto.

---
+++ MVC dotnet/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_dotnet.Models
+{
+    public class Expense
+    {
+        public int id { get; set; }
+        public decimal Value { get; set; }
+        [Required] public string? Description { get; set; }
+    }
+}
---
+++ MVC dotnet/Models/ExpensesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_dotnet.Models
+{
+    public class ExpensesDbContext: DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public ExpensesDbContext(DbContextOptions<ExpensesDbContext> options)
+            : base(options)
+        { 
+            
+        }
+    }
+}
